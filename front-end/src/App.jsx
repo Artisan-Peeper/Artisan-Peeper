95d9985011b8564bf0307066b42f576698a23197
MasterTraits <emmanuelfabella606@gmail.com>
2024-03-29 20:40:41 +0800

    Fixed merge conflict

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ApplicantFooter from './components/ApplicantFooter.jsx'
-import EmployerFooter from './components/EmployerFooter.jsx';
-import ErrorMSG from './components/error.jsx'
-
-const App = () => {
-  return (
-    <>
-      
-      <div className="h-screen w-screen flex justify-center items-center">
-        <ErrorMSG/>
-      </div>
-      {/* <ApplicantFooter /> */}
-      {/* <EmployerFooter /> */}
-    </>
-=======
 import { useState } from "react";
 import {
   Route,
